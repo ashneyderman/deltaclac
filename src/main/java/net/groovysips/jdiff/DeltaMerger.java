11134a062645993369f153e7c28582986ff783e7
a.shneyderman <a.shneyderman@74faef92-4084-11de-9a54-b9394989f659>
2009-05-14 13:40:54 +0000

    Initial version of jdiff.

---
+++ src/main/java/net/groovysips/jdiff/DeltaMerger.java
@@ -0,0 +1,14 @@
+package net.groovysips.jdiff;
+
+
+/**
+ * TODO (Shneyderman - May 13, 2009) : description
+ *
+ * @author Shneyderman
+ */
+public interface DeltaMerger
+{
+
+    Object merge( Object object, Delta delta );
+
+}
