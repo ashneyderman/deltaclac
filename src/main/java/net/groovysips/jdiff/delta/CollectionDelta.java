152ba1032aa880860d15e9581d1baabd4de41d36
a.shneyderman <a.shneyderman@74faef92-4084-11de-9a54-b9394989f659>
2009-05-14 13:39:14 +0000

    Initial version of jdiff.

---
+++ src/main/java/net/groovysips/jdiff/delta/CollectionDelta.java
@@ -0,0 +1,24 @@
+package net.groovysips.jdiff.delta;
+
+import net.groovysips.jdiff.CompositeDelta;
+
+/**
+ * TODO: provide javadoc.
+ *
+ * @author Shneyderman
+ */
+public class CollectionDelta extends AbstractCompositeDelta implements CompositeDelta
+{
+
+    /**
+     * A collections delta might have to do the following:
+     *
+     *   1. Create a collection and assign it to the target field.
+     *   2. 
+     *
+     */
+
+
+
+
+}
