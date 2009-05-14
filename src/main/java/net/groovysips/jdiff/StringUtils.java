9a6bdce43388b78a9a2d8019c65776081c8dca86
a.shneyderman <a.shneyderman@74faef92-4084-11de-9a54-b9394989f659>
2009-05-14 13:41:14 +0000

    Initial version of jdiff.

---
+++ src/main/java/net/groovysips/jdiff/StringUtils.java
@@ -0,0 +1,16 @@
+package net.groovysips.jdiff;
+
+/**
+ * TODO: provide javadoc.
+ *
+ * @author Shneyderman
+ */
+public class StringUtils
+{
+
+    public static final String buildLogableString(String msg, Object[][] args)
+    {
+        return "";
+    }
+
+}
